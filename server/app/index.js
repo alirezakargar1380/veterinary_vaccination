f972d549a6e7b37787314010dcfcb87c535d5694
alireza kargar <imanesche@gmail.com>
2021-08-25 20:39:50 +0430

    api authentication

--- server/app/index.js
+++ server/app/index.js
@@ -1,6 +1,10 @@
 const express = require("express")
 const app = express();
 
+// authentication
+const auth_controller = require("./controller/basic_authentication.controller")
+app.use(auth_controller.API_authenticaiton)
+
 app.use(express.json())
 require("./routers")(app)
 
