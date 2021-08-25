23d4bf4cec57f35bbb1ea66a7084a8bc4a162d08
alireza kargar <imanesche@gmail.com>
2021-08-25 23:10:57 +0430

    Update veterinary.sql

--- server/backup/veterinary.sql
+++ server/backup/veterinary.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 25, 2021 at 08:36 PM
+-- Generation Time: Aug 25, 2021 at 08:40 PM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.7
 
@@ -107,7 +107,6 @@ CREATE TABLE `users` (
   `phone` varchar(20) COLLATE utf8_bin NOT NULL,
   `name` varchar(100) COLLATE utf8_bin NOT NULL,
   `lastname` varchar(100) COLLATE utf8_bin NOT NULL,
-  `username` varchar(1000) COLLATE utf8_bin NOT NULL,
   `password` varchar(1000) COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
