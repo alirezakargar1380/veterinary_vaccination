957b6a9d9b3e68d759a7da20ecb2dbc4cd83ec03
alireza kargar <imanesche@gmail.com>
2021-08-26 19:05:13 +0430

    remove route vacc projects

--- server/app/routers/index.js
+++ server/app/routers/index.js
@@ -4,6 +4,7 @@ module.exports = (app) => {
       require("./users.routers"),
       require("./test.router"),
       require("./livestock.routers"),
-      require("./vaccination_projects.routers"),
+      require("./livestock_types.routers"),
+      require("./livestock_information.routers"),
   )
 }
