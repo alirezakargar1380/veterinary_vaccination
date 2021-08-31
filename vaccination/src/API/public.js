5b9eecefedc631993aee5b2fd09ba880441a1c42
alireza kargar <imanesche@gmail.com>
2021-09-01 03:03:07 +0430

    Update public.js

--- vaccination/src/API/public.js
+++ vaccination/src/API/public.js
@@ -15,12 +15,36 @@ module.exports.add_livestock = async (body) => {
   return await axios.post(BASE_URL+'/livestock/add', body, headers)
 }
 
+module.exports.add_vaccines = async (body) => {
+  return await axios.post(BASE_URL+'/vaccines/add', body, headers)
+}
+
+module.exports.add_address = async (body) => {
+  return await axios.post(BASE_URL+'/address/add', body, headers)
+}
+
 module.exports.get_user = async () => {
   return await axios.get(BASE_URL+'/get_users', headers)
 }
 
+module.exports.get_address = async () => {
+  return await axios.get(BASE_URL+'/address/get', headers)
+}
+
+module.exports.get_vaccines = async () => {
+  return await axios.get(BASE_URL+'/vaccines/get', headers)
+}
+
 module.exports.get_livestock = async () => {
   return await axios.get(BASE_URL+'/livestock/get', headers)
+}
+
+module.exports.get_life_stock_types = async () => {
+  return await axios.get(BASE_URL+'/livestock_types/get', headers)
+}
+
+module.exports.get_life_stock_information = async () => {
+  return await axios.get(BASE_URL+'/livestock_information/get', headers)
 }
 
 module.exports.get_address = async () => {
