e98d5e5a73c53ca72c039ab473cfcaddd9a2f7f8
alireza kargar <imanesche@gmail.com>
2021-09-01 03:05:21 +0430

    add section to list componnets

--- vaccination/src/components/List/List.jsx
+++ vaccination/src/components/List/List.jsx
@@ -53,25 +53,85 @@ export default class List extends React.Component {
           })
         })
         break;
+      case 'information_livestock':
+        this.setState({
+          title: [
+            'شناسه',
+            'نام',
+            'نام خانوادگی',
+            'کد ملی',
+            'نام پدر',
+            'نوع دام',
+            'آدرس محل تحت پوشش',
+            'استان',
+            'شهر',
+            'شماره شناسنامه'
+          ]
+        })
+        await api.get_life_stock_information().then((res) => {
+          for (let i = 0; i < res.data.data.length; i++)
+          {
+            res.data.data[i].name = res.data.data[i].livestock.name
+            res.data.data[i].lastname = res.data.data[i].livestock.lastname
+            res.data.data[i].natinal_id = res.data.data[i].livestock.natinal_id
+            res.data.data[i].father = res.data.data[i].livestock.father
+            delete res.data.data[i].livestock
+            delete res.data.data[i].livestock_id
+            console.log(res.data.data[i])
+          }
+
+          this.setState({
+            data: res.data.data
+          })
+        })
+        break;
+      case 'vaccines':
+        this.setState({
+          title: [
+            'شناسه',
+            'نام واکسن',
+            'سریال'
+          ]
+        })
+        await api.get_vaccines().then((res) => {
+          this.setState({
+            data: res.data.data
+          })
+        })
+        break;
+      case 'lifestock_address':
+        this.setState({
+          title: [
+            'شناسه',
+            'آدرس'
+          ]
+        })
+        await api.get_address().then((res) => {
+          this.setState({
+            data: res.data.data
+          })
+        })
+        break;
       default:
         console.log('def')
     }
 
+    if (!this.state.data[0]) return
     var title = Object.keys(this.state.data[0])
-    title.splice(title.length-3,3)
-    console.log(title)
     this.setState({
       keys: title
     })
+
   }
+
 
   render() {
     const {data, title, keys} = this.state
     // const {hello} = this.props
     // console.log(title)
-    if (data.length === 0) return <h1>loading</h1>
+    if (data.length === 0) return <h1>loading | emty</h1>
     return(
-        <div>
+        <div className="w-fit mx-auto">
           <table border="1" className="font-Thin f-20" dir='rtl'>
             <thead>
             <tr>
