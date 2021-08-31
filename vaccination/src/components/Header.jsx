f661290ea14685e0a89bc56e8052e5e8aaaf952f
alireza kargar <imanesche@gmail.com>
2021-09-01 03:03:34 +0430

    add button to header component

--- vaccination/src/components/Header.jsx
+++ vaccination/src/components/Header.jsx
@@ -5,10 +5,12 @@ export default class Header extends React.Component {
    render() {
      return(
          <div className="text-center">
-           <SectionButton value={"کارمند ها"} class={"mx-4"}/>
-           <SectionButton value={"واکسن ها"} class={"mx-4"}/>
-           <SectionButton value={"کارمند ها"} class={"mx-4"}/>
-           <SectionButton value={"کارمند ها"} class={"mx-4"}/>
+           <SectionButton href={"/add_employee"} value={"کارمند ها"} class={"mx-4"}/>
+           <SectionButton href={"/add_live_stock"} value={"دام دار ها"} class={"mx-4"}/>
+           <SectionButton href={"/information_livestock"} value={"اطلاعات دام ها"} class={"mx-4"}/>
+           <SectionButton href={"/lifestock_address"} value={"آدرس مراکز"} class={"mx-4"}/>
+           <SectionButton href={"/"} value={"پرینت"} class={"mx-4"}/>
+           <SectionButton href={"/vaccines"} value={"واکسن"} class={"mx-4"}/>
          </div>
      )
    }
