248e3e981cbf3049a4e9d137b1d95cf34dd0c037
alireza kargar <imanesche@gmail.com>
2021-09-01 03:03:01 +0430

    add tag a with href to BTN component

--- vaccination/src/components/BTN/Section_btn.jsx
+++ vaccination/src/components/BTN/Section_btn.jsx
@@ -3,11 +3,13 @@ import React from "react";
 export default class SectionButton extends React.Component {
   render() {
     return(
-        <button
-            className={"radius-10 section-btn-shodow w-fit bg-w cursor border-0 "+this.props.class}
-        >
-          <div className="px-3 font-Thin f-20">{this.props.value}</div>
-        </button>
+        <a href={this.props.href}>
+          <button
+              className={"radius-10 section-btn-shodow w-fit bg-w cursor border-0 "+this.props.class}
+          >
+            <div className="px-3 font-Thin f-20">{this.props.value}</div>
+          </button>
+        </a>
     )
   }
 }
